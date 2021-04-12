7bb4900a873435ac8f6ed055f2d86849381a5819
Ivan Alieksieienko <alexeenko20@gmail.com>
2021-04-12 20:00:03 +0300

    Finished setup on mac environment, added posibillity to use mysql instead of mssql

---
+++ Plants.Core/DatabaseScripts/MySqlDbScripts.sql
@@ -0,0 +1,30 @@
+﻿CREATE DATABASE Plants;
+
+CREATE TABLE Product
+(
+		ID CHAR(36) NOT NULL PRIMARY KEY,
+		CategoryID CHAR(36) NOT NULL,
+		IsAvailable BOOL,
+		Name TEXT,
+		Description TEXT,
+		ImagePath TEXT,
+		Price INT
+);
+
+CREATE TABLE Category
+(
+	ID CHAR(36) NOT NULL PRIMARY KEY,
+	Name TEXT,
+	Description TEXT,
+	ImagePath TEXT
+);
+
+CREATE TABLE Admin
+(
+	ID CHAR(36) NOT NULL PRIMARY KEY,
+	Login TEXT,
+	Password TEXT
+);
+
+INSERT INTO Admin (ID, Login, Password)
+VALUES ('cb5cc5cd-085d-4a04-94df-2780aa0b150e', 'Moroshan', 'moroztaushan23');
